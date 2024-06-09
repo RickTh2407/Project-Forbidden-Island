c4b9be496a7b986b443dcc9f51b2a5b3d59d3b6c
RickTh <rick.thewissen@gmail.com>
2024-06-09 19:27:27 +0200

    09-06-2024

--- Forbidden Island/obj/Debug/net8.0/Forbidden Island.AssemblyInfo.cs
+++ Forbidden Island/obj/Debug/net8.0/Forbidden Island.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Forbidden Island")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a562b4e574baf59b93235bafb9437775ce088189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b0ebe61c5347bdbab0e65ea599038713322f78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Forbidden Island")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Forbidden Island")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
